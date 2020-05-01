edb27f6d33298ff62476275f1cbda1de47bd8b10
Ril <Ril@DESKTOP-IQV4RP2>
2020-05-01 20:01:21 +0530

    Created Thread using Lambda Expressions

--- Java8/src/com/java/lambda/LambdaExpression.java
+++ Java8/src/com/java/lambda/LambdaExpression.java
@@ -1,5 +1,18 @@
 package com.java.lambda;
 
 public class LambdaExpression {
-
+	
+	public static void main(String[] args) {
+		
+		Runnable runnable=()->{
+			for(int i=0;i<10;i++) {
+				System.out.println("from child");
+			}
+		};
+		
+		Thread thread=new Thread(runnable);
+		thread.start();
+		System.out.println("from main");
+		
+	}
 }
